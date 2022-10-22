477307b5fd1929bdbd87acbbd56196126fe7d1ab
unknown <sifatniloy18@gmail.com>
2022-10-22 13:02:45 -0600

    all things recap started

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h4>The page you are looking for is not found</h4>
+        </div>
+    );
+};
+
+export default NotFound;
